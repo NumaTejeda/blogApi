b539ccca475b536b3df687377d808bd89c323246
NumaDev <at.psicotrisquel@gmail.com>
2024-08-15 16:06:17 -0300

    Frist Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+// import routes from './routes/index.js'
+import cookieParser from 'cookie-parser'; // Para manejar cookies
+
+
+const port = 3100;
+
+
+const app = express();
+
+// Middleware para manejar cookies
+app.use(cookieParser());
+
+//Middleware para parsear el cuerpo de la solicitud
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/', routes)
+
+app.listen(port, () => {
+    console.log(`Abrime => http://localhost:${port}`);
+})
